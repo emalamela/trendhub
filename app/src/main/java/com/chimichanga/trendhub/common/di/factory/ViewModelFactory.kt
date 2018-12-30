faba48fb9ca921ed7a09381bdd0329f70aada82b
emalamela <lamelaemanuel@gmail.com>
2018-12-30 12:37:56 -0300

    Document ViewModelFactory and ApplicationGlideModule

--- app/src/main/java/com/chimichanga/trendhub/common/di/factory/ViewModelFactory.kt
+++ app/src/main/java/com/chimichanga/trendhub/common/di/factory/ViewModelFactory.kt
@@ -7,12 +7,28 @@ import javax.inject.Inject
 import javax.inject.Provider
 import javax.inject.Singleton
 
+/**
+ * Backed by `Dagger`s Multibinding [Map], this factory [create]s [ViewModel] instances by retrieving its corresponding
+ * [Provider] instance and executing it.
+ */
 @Singleton
 class ViewModelFactory
 @Inject constructor(
     private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
 
+    /**
+     * Creates a [ViewModel] instance of type [T] by finding its corresponding [Provider]
+     *
+     * @param T the type of [ViewModel] to create
+     * @param modelClass the [Class] of the type [T]
+     *
+     * @return an instance of [T]
+     *
+     * @throws IllegalArgumentException if no [Provider] is found for such [T]
+     *
+     * @see [com.chimichanga.trendhub.common.di.module.ViewModelFactoryModule]
+     */
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         val creator = creators[modelClass] ?:
         creators.asIterable().firstOrNull { modelClass.isAssignableFrom(it.key) }?.value
--- app/src/main/java/com/chimichanga/trendhub/common/glide/ApplicationGlideModule.kt
+++ app/src/main/java/com/chimichanga/trendhub/common/glide/ApplicationGlideModule.kt
@@ -3,5 +3,8 @@ package com.chimichanga.trendhub.common.glide
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
 
+/**
+ * Subclass of [AppGlideModule] needed for [GlideApp] to be generated
+ */
 @GlideModule
 class ApplicationGlideModule : AppGlideModule()
