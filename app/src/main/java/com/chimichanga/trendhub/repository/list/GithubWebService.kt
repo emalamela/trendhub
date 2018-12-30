599ff8505e015015f4733bd2ef41c2d853e7d83e
emalamela <lamelaemanuel@gmail.com>
2018-12-30 15:32:25 -0300

    Display 30 repositories

--- app/src/main/java/com/chimichanga/trendhub/repository/list/GithubWebService.kt
+++ app/src/main/java/com/chimichanga/trendhub/repository/list/GithubWebService.kt
@@ -9,7 +9,7 @@ import retrofit2.http.Headers
 interface GithubWebService {
 
     @Headers("Accept: application/vnd.github.mercy-preview+json")
-    @GET("search/repositories?q=topic:android&page=1&per_page=10")
+    @GET("search/repositories?q=topic:android&page=1&per_page=30")
     fun fetchAndroidTrendingRepositories(): Deferred<Page<Repository>>
 
 }
